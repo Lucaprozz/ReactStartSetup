c30e6270f92f80f87d6fcca6d15fdd24d7fef851
Lucaprozz <1038179@idcollege.nl>
2018-11-12 20:53:20 +0100

    everything configured with webpack

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Testje from './components/testje/Testje'
+import './styles/styles.scss'
+class App extends Component {
+  render() {
+      return (
+          <>
+            <Testje />
+          </>
+      );
+  }
+}
+
+export default App;
---
+++ src/components/testje/Testje.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Test extends Component{
+    render(){
+        return(
+            <>
+            <p className='paratestje'>Dit is een testje</p>
+            </>
+        )
+    }
+}
+
+export default Test;
