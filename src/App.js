29611bbc3e670fdac64404ed6fd13e8a32596d39
Lucaprozz <1038179@idcollege.nl>
2018-11-13 21:22:56 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import Testje from './components/testje/Testje'
+import { BrowserRouter , Switch, Route, Redirect } from 'react-router-dom';
+import Home from './routes/Home'
+import About from './routes/About'
 import './styles/styles.scss'
+
 class App extends Component {
   render() {
       return (
-          <>
-            <Testje />
-          </>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home}></Route>
+                <Route path='/About' component={About}></Route>
+                <Redirect from='*' to='/' />
+            </Switch>
+        </BrowserRouter>
       );
-  }
+    }
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from '../components/header/Header'
+
+
+class About extends Component{
+    render(){
+        return(
+            <>
+                <Header />
+                <h1>About Page </h1>
+            </>
+        )
+    }
+}
+
+export default About;
